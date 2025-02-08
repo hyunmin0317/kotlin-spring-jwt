962453c27f4cf1610dcd4e962af0b0e60efd61ab
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-08 10:49:43 +0900

    feat: 프로젝트 생성 (#1)

---
+++ src/test/kotlin/com/hyunmin/kopring/KotlinSpringJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyunmin.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
