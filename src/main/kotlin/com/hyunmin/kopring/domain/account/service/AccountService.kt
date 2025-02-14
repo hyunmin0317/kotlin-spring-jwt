147afa1b368e1e66a1f8021559b20caf4500a2e1
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-14 18:29:12 +0900

    feat: 회원가입 validation 추가 (#3)

--- src/main/kotlin/com/hyunmin/kopring/domain/account/service/AccountService.kt
+++ src/main/kotlin/com/hyunmin/kopring/domain/account/service/AccountService.kt
@@ -4,6 +4,8 @@ import com.hyunmin.kopring.domain.account.dto.RegisterRequest
 import com.hyunmin.kopring.domain.account.dto.RegisterResponse
 import com.hyunmin.kopring.global.common.entity.Member
 import com.hyunmin.kopring.global.common.repository.MemberRepository
+import com.hyunmin.kopring.global.exception.GeneralException
+import com.hyunmin.kopring.global.exception.code.ErrorCode
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -14,9 +16,16 @@ class AccountService(
 ) {
 
     fun register(request: RegisterRequest): RegisterResponse {
+        validateUsername(request.username)
         val encodedPw = passwordEncoder.encode(request.password)
         val member = Member(username = request.username, password = encodedPw)
         val saved = memberRepository.save(member)
         return RegisterResponse(saved.id!!, saved.username, saved.role, saved.createdAt)
+    }
+
+    private fun validateUsername(username: String) {
+        if (memberRepository.existsByUsername(username)) {
+            throw GeneralException(ErrorCode.ACCOUNT_CONFLICT)
+        }
     }
 }
--- src/main/kotlin/com/hyunmin/kopring/global/common/repository/MemberRepository.kt
+++ src/main/kotlin/com/hyunmin/kopring/global/common/repository/MemberRepository.kt
@@ -3,4 +3,7 @@ package com.hyunmin.kopring.global.common.repository
 import com.hyunmin.kopring.global.common.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long> {
+
+    fun existsByUsername(username: String): Boolean
+}
