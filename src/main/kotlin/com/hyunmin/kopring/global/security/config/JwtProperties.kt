cae4e39bf2fc9dfb8f7b752e15dda202c7ad9c85
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-23 19:57:00 +0900

    chore: JWT 관련 상수 설정 (#10)

---
+++ src/main/kotlin/com/hyunmin/kopring/global/security/config/JwtProperties.kt
@@ -0,0 +1,32 @@
+package com.hyunmin.kopring.global.security.config
+
+import io.jsonwebtoken.Jwts
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    var secret: String = "",
+    var token: TokenProperties = TokenProperties()
+) {
+
+    data class TokenProperties(
+        var accessExpirationTime: Long = 0,
+        var refreshExpirationTime: Long = 0
+    )
+
+    // 비밀 키 생성
+    fun getSecretKey(): SecretKey {
+        val keyBytes = secret.toByteArray(StandardCharsets.UTF_8)
+        return SecretKeySpec(keyBytes, Jwts.SIG.HS256.key().build().algorithm)
+    }
+
+    // JWT 토큰의 만료 시간을 반환
+    fun getExpirationTime(isRefresh: Boolean): Long {
+        return if (isRefresh) token.refreshExpirationTime else token.accessExpirationTime
+    }
+}
