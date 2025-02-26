1c3da336fb8142e39ea9393e11559c139a76ac14
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-26 22:37:38 +0900

    refactor: 접근 제어 수준 변경 (#31)

--- src/main/kotlin/com/hyunmin/kopring/global/exception/handler/GeneralExceptionHandler.kt
+++ src/main/kotlin/com/hyunmin/kopring/global/exception/handler/GeneralExceptionHandler.kt
@@ -19,42 +19,42 @@ class GeneralExceptionHandler {
 
     // 사용자 정의 예외(GeneralException) 처리 메서드
     @ExceptionHandler(GeneralException::class)
-    fun handleGeneralException(ex: GeneralException): ResponseEntity<ErrorResponse<Void>> {
+    protected fun handleGeneralException(ex: GeneralException): ResponseEntity<ErrorResponse<Void>> {
         log.warn("[WARNING] {} : {}", ex.javaClass, ex.message)
         return ErrorResponse.handle(ex.errorCode)
     }
 
     // 요청 파라미터 검증 실패(MethodArgumentNotValidException) 처리 메서드
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse<Map<String, String>>> {
+    protected fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse<Map<String, String>>> {
         log.warn("[WARNING] {} : {}", ex.javaClass, ex.message)
         return ErrorResponse.handle(ErrorCode.VALIDATION_FAILED, ex.fieldErrors)
     }
 
     // 데이터 무결성 위반(DataIntegrityViolationException) 처리 메서드
     @ExceptionHandler(DataIntegrityViolationException::class)
-    fun handleDataIntegrityViolationException(ex: DataIntegrityViolationException): ResponseEntity<ErrorResponse<Void>> {
+    protected fun handleDataIntegrityViolationException(ex: DataIntegrityViolationException): ResponseEntity<ErrorResponse<Void>> {
         log.warn("[WARNING] {} : {}", ex.javaClass, ex.message)
         return ErrorResponse.handle(ErrorCode.VALIDATION_FAILED)
     }
 
     // 컨트롤러 메서드 파라미터의 유효성 검증 실패(HandlerMethodValidationException) 처리 메서드 - @PermissionCheckValidator
     @ExceptionHandler(HandlerMethodValidationException::class)
-    fun handleHandlerMethodValidationException(ex: HandlerMethodValidationException): ResponseEntity<ErrorResponse<Void>> {
+    protected fun handleHandlerMethodValidationException(ex: HandlerMethodValidationException): ResponseEntity<ErrorResponse<Void>> {
         log.warn("[WARNING] {} : {}", ex.javaClass, ex.message)
         return ErrorResponse.handle(extractErrorCode(ex))
     }
 
     // 지원되지 않는 HTTP 메서드 요청(HttpRequestMethodNotSupportedException) 처리 메서드
     @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
-    fun handleHttpRequestMethodNotSupportedException(ex: HttpRequestMethodNotSupportedException): ResponseEntity<ErrorResponse<Void>> {
+    protected fun handleHttpRequestMethodNotSupportedException(ex: HttpRequestMethodNotSupportedException): ResponseEntity<ErrorResponse<Void>> {
         log.warn("[WARNING] {} : {}", ex.javaClass, ex.message)
         return ErrorResponse.handle(ErrorCode.METHOD_NOT_ALLOWED)
     }
 
     // 기타 모든 예외(Exception) 처리 메서드
     @ExceptionHandler(Exception::class)
-    fun handleException(ex: Exception): ResponseEntity<ErrorResponse<Void>> {
+    protected fun handleException(ex: Exception): ResponseEntity<ErrorResponse<Void>> {
         log.error("[ERROR] {} : {}", ex.javaClass, ex.message, ex)
         return ErrorResponse.handle(ErrorCode.INTERNAL_SERVER_ERROR)
     }
