e002b797cd7ee3bdcdfc6e994e16757526d44102
Hyunmin Choi <hyunmin-choi@naver.com>
2025-02-26 21:33:39 +0900

    [FEATURE] 특정 사용자 정보 조회 API (#27)
    * chore: 사용자 권한 확인 어노테이션 (#20)
    
    * delete: 사용자 권한 확인 어노테이션 (#20)
    
    * feat: 특정 사용자 정보 조회 API (#20)
    
    * chore: 관리자 권한 설정 (#20)

--- src/main/kotlin/com/hyunmin/kopring/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/hyunmin/kopring/domain/member/controller/MemberController.kt
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,5 +21,11 @@ class MemberController(
     fun findAll(@ParameterObject pageable: Pageable): ResponseEntity<Page<MemberInfoResponse>> {
         val responsePage = memberQueryService.findAll(pageable)
         return ResponseEntity.ok(responsePage)
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<MemberInfoResponse> {
+        val response = memberQueryService.findById(id)
+        return ResponseEntity.ok(response)
     }
 }
--- src/main/kotlin/com/hyunmin/kopring/domain/member/service/MemberQueryService.kt
+++ src/main/kotlin/com/hyunmin/kopring/domain/member/service/MemberQueryService.kt
@@ -2,6 +2,8 @@ package com.hyunmin.kopring.domain.member.service
 
 import com.hyunmin.kopring.domain.member.dto.MemberInfoResponse
 import com.hyunmin.kopring.global.common.repository.MemberRepository
+import com.hyunmin.kopring.global.exception.GeneralException
+import com.hyunmin.kopring.global.exception.code.ErrorCode
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -16,5 +18,11 @@ class MemberQueryService(
     fun findAll(pageable: Pageable): Page<MemberInfoResponse> {
         val memberPage = memberRepository.findAll(pageable)
         return memberPage.map { MemberInfoResponse(it.id!!, it.username, it.role, it.createdAt, it.updatedAt) }
+    }
+
+    fun findById(id: Long): MemberInfoResponse {
+        val member = memberRepository.findById(id)
+            .orElseThrow { GeneralException(ErrorCode.MEMBER_NOT_FOUND) }
+        return MemberInfoResponse(member.id!!, member.username, member.role, member.createdAt, member.updatedAt)
     }
 }
